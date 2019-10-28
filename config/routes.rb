41038c5460db795cd592b3a78959c24d6d573651
Michael Stark <michael.stark@sportdeutschland.tv>
2019-10-28 14:02:45 +0100

    adds new resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   post "/api/resources/:klass/:uuid" => "api/resources#create"
   delete "/api/resources/:klass/:uuid" => "api/resources#destroy"
   put "/api/resources/:klass/:uuid" => "api/resources#update"
+
+  post "/backend/api/resources/:klass/:uuid" => "api/resources#create"
+  delete "/backend/api/resources/:klass/:uuid" => "api/resources#destroy"
+  put "/backend/api/resources/:klass/:uuid" => "api/resources#update"
 end
